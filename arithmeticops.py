e08fcbfa4ad86af721c5e390410e930d7ac729dc
Palak Singh <palak.singh0392@gmail.com>
2026-06-27 14:39:29 +0530

    Add basic arithmetic operations in arithmeticops.py

---
+++ arithmeticops.py
@@ -0,0 +1,12 @@
+#various operations 
+#userinput
+a = int(input("enter the value of a:"))
+b = int(input("enter the value of b:"))
+add= a+b  #addition
+sub= a-b  #substraction
+mul= a*b  #multiply
+div = a/b  #divison
+print("the addition of two numbers are:", add)
+print("the substraction of two numbers are:", sub)
+print("the multiplication of two numbers are:", mul)
+print("the divison of two numbers are:", div)

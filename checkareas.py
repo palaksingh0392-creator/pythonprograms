88fcb6b646d5566ff8df56b8e584a489b1c18ba0
Palak Singh <palak.singh0392@gmail.com>
2026-06-28 12:36:59 +0530

    Update area calculations for square, rectangle, and triangle

--- checkareas.py
+++ checkareas.py
@@ -1,18 +1,16 @@
 #find areas of following:-
 a = int(input("enter the value of a:"))
-l= int(input("enter the value of l:"))
 b= int(input("enter the value of b:"))
-r = int(input("enter the value of r:"))
-h = int(input("enter the value of h:"))
+
 #square
 squ=a*a
 print("the area of square is:",squ)
 #rectangle
-rec= l*b
+rec= a*b
 print("the area of rectangle is:",rec)
 #circle
-cir = 3.15*r*r
+cir = 3.15*a*a
 print("the area of circle is:",cir)
 #triangle
-tri = 0.5*l*h
+tri = 0.5*b*a
 print("the area of triangle is:",tri)

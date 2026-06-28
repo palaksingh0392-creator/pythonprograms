725816761441dca4d977f680661f63e73e868168
Palak Singh <palak.singh0392@gmail.com>
2026-06-28 12:33:51 +0530

    add input prompts in perimetercheck.py
    add  input prompts for variables a, l, b, and r.

---
+++ perimetercheck.py
@@ -0,0 +1,18 @@
+#find perimeter of following:-
+a = int(input("enter the value of a:")) #one side
+l= int(input("enter the value of l:"))  #length
+b= int(input("enter the value of b:"))  #breadth
+r = int(input("enter the value of r:"))  #radius
+
+#square
+squ=4*a
+print("the area of square is:",squ)
+#rectangle
+rec= 2*(l+b)
+print("the area of rectangle is:",rec)
+#circle
+cir = 3.15*2*r
+print("the area of circle is:",cir)
+#triangle
+tri = a+a+a
+print("the area of triangle is:",tri)

b27cb3642513163e293b58979b6438e723671939
Palak Singh <palak.singh0392@gmail.com>
2026-06-28 12:18:22 +0530

    Add negative number check in negnumcheck.py

---
+++ negnumcheck.py
@@ -0,0 +1,7 @@
+a = int(input("enter the value of a:"))
+if a < 0:
+    print("this is negative number")
+elif a > 0:
+        print("this is positive number")
+else :
+            print("this is zero value")

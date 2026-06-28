1cc55fb6906c3c4c1b611ba038d7c9c6e203b548
Palak Singh <palak.singh0392@gmail.com>
2026-06-28 13:03:13 +0530

    Add comparison logic for two numbers

---
+++ comaretwonum.py
@@ -0,0 +1,9 @@
+num1 = int(input("enter the value of num1: "))
+num2 = int(input("enter the value of num2: "))
+
+if num1 < num2 :
+    print("the num2 is greater than num1")
+elif num1 > num2 :
+    print("the num1 is greater than num2")
+else :
+    print("they are equal")

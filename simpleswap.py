2f7c08923f60410ad4ece2c23354f59bca842bff
Palak Singh <palak.singh0392@gmail.com>
2026-06-28 12:44:51 +0530

    Implement simple swap of two variables in Python

---
+++ simpleswap.py
@@ -0,0 +1,11 @@
+# Take input
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+
+# Swap using a third variable
+c = a
+a = b
+b = c
+
+print("The value of a after swapping:", a)
+print("The value of b after swapping:", b)

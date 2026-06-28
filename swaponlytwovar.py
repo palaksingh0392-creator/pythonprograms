0a0d7ea506c53f58ce8d5e3913f672d7e80ef188
Palak Singh <palak.singh0392@gmail.com>
2026-06-28 12:45:37 +0530

    Implement swapping of two variables in Python

---
+++ swaponlytwovar.py
@@ -0,0 +1,9 @@
+# Take input
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+
+# Swap using a third variable
+a,b = b,a
+
+print("The value of a after swapping:", a)
+print("The value of b after swapping:", b)

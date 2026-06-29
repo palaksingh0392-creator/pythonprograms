d211c7028a63cfbb59d69a9cfdff6dee1d3abd03
Palak Singh <palak.singh0392@gmail.com>
2026-06-29 15:49:01 +0530

    Implement switch-case for days of the week

---
+++ days_of_week_switch.py
@@ -0,0 +1,25 @@
+# take input
+day = int(input("Enter the day: "))
+
+# logic
+def get_status(day):
+    match day:
+        case 1:
+            return "Monday"
+        case 2:
+            return "Tuesday"
+        case 3:
+            return "Wednesday"
+        case 4:
+            return "Thursday"
+        case 5:
+            return "Friday"
+        case 6:
+            return "Saturday"
+        case 7:
+            return "Sunday"
+        case _:
+            return "Unknown Status"
+
+# function call
+print(get_status(day))
